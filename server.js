a59608c912ccb44b1c239ca2c286425946e46d9c
govindumeesala <meesalagovindu123@gmail.com>
2025-03-24 19:49:44 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
