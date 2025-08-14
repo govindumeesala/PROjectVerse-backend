bc85d060b2c11c933fb31a4248fd52401cc038eb
kmssudheer <sudheerkalyanam44@gmail.com>
2025-08-14 12:18:05 +0530

    Standardizes project status enum to lowercase

--- src/models/Project.js
+++ src/models/Project.js
@@ -9,7 +9,7 @@ const projectSchema = new mongoose.Schema({
   githubURL: { type: String, required: false },
   deploymentURL: { type: String, required: false },
   demoURL : {type:String,required:false},
-  status: { type: String, enum: ["completed", "Ongoing"], default: "Ongoing" },
+  status: { type: String, enum: ["completed", "ongoing"], default: "ongoing" },
   lookingForContributors: { type: Boolean, default: false }, // Whether the project is looking for contributors
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Project owner
   contributors: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Contributors
